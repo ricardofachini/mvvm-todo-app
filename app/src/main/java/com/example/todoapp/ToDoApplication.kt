a6b2fd231a38ce1c61b03b929ce0a4a207c834e2
Ricardo Fachini <ricardofachinisouza@gmail.com>
2022-03-19 00:28:14 -0300

    add koin module

--- app/src/main/java/com/example/todoapp/ToDoApplication.kt
+++ app/src/main/java/com/example/todoapp/ToDoApplication.kt
@@ -1,12 +1,20 @@
 package com.example.todoapp
 
 import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
 
 
 class ToDoApplication: Application() {
 
     override fun onCreate() {
         super.onCreate()
-        startKoin
+        //start Koin
+        startKoin {
+            androidLogger()
+            androidContext(this@ToDoApplication)
+            modules()
+        }
     }
 }
---
+++ app/src/main/java/com/example/todoapp/utils/AppModule.kt
@@ -0,0 +1,7 @@
+package com.example.todoapp.utils
+
+import org.koin.dsl.module
+
+object AppModule {
+    private val appModule = module {  }
+}
