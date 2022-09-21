ff9fe919bfecf078e2a25ffee1105e5496d7a54c
Ricardo Fachini <ricardofachinisouza@gmail.com>
2022-09-20 21:17:18 -0300

    add task unit test case

---
+++ domain/src/test/java/com/example/domain/usecase/AddTaskUseCaseTest.kt
@@ -0,0 +1,39 @@
+package com.example.domain.usecase
+
+import com.example.domain.model.Task
+import com.example.domain.repository.ITaskRepositoryLocal
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.whenever
+
+class AddTaskUseCaseTest {
+    private lateinit var useCase: AddTaskUseCase
+
+    @Before
+    fun setupTest() {
+        val mockITaskRepositoryLocal: ITaskRepositoryLocal = mock()
+        runBlocking {
+            whenever(mockITaskRepositoryLocal.insertNewTask(Task(
+                id = 0,
+                title = "tarefa",
+                isDone = false
+            ))).thenReturn(true)
+        }
+        useCase = AddTaskUseCase(mockITaskRepositoryLocal)
+    }
+
+    @Test
+    fun `test add new task`() {
+        runBlocking {
+            val result = useCase.invoke()
+            if (!result) {
+                fail()
+            } else {
+                assert(true)
+            }
+        }
+    }
+}
