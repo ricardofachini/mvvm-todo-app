29036cc55798afcdd8dba1b4969747890c4b6867
Ricardo Fachini <ricardofachinisouza@gmail.com>
2023-01-17 14:53:36 -0300

    primarykey as autogenerate

--- data/src/main/java/com/example/data/local/entity/TaskLocal.kt
+++ data/src/main/java/com/example/data/local/entity/TaskLocal.kt
@@ -4,9 +4,9 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity
+@Entity(tableName = "task")
 data class TaskLocal(
-    @PrimaryKey val id: Int,
-    @ColumnInfo(name = "title") val title: String?,
+    @PrimaryKey(autoGenerate = true) @ColumnInfo(name = "id") val id: Int,
+    @ColumnInfo(name = "title") val title: String,
     @ColumnInfo(name = "is_done") val isDone: Boolean
 )
