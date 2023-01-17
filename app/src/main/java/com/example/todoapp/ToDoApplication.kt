134aafc35b7e8a49e7f7cc6b933c48e2bd76a251
Ricardo Fachini <ricardofachinisouza@gmail.com>
2023-01-17 14:54:58 -0300

    add hilt on main application class and remove koin

--- app/src/main/java/com/example/todoapp/ToDoApplication.kt
+++ app/src/main/java/com/example/todoapp/ToDoApplication.kt
@@ -1,21 +1,12 @@
 package com.example.todoapp
 
 import android.app.Application
-import com.example.todoapp.utils.AppModule.appModule
-import org.koin.android.ext.koin.androidContext
-import org.koin.android.ext.koin.androidLogger
-import org.koin.core.component.KoinComponent
-import org.koin.core.context.startKoin
+import dagger.hilt.android.HiltAndroidApp
 
-
+@HiltAndroidApp
 class ToDoApplication: Application() {
 
     override fun onCreate() {
         super.onCreate()
-        //start Koin
-        startKoin {
-            androidContext(this@ToDoApplication)
-            modules(appModule)
-        }
     }
 }
