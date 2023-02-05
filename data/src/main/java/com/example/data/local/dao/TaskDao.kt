146b7f340dbdc092cce632c95dab4c99fbd23bab
Ricardo Fachini <ricardofachinisouza@gmail.com>
2023-02-04 21:57:27 -0300

    implements get all tasks from the DB

--- data/src/main/java/com/example/data/local/dao/TaskDao.kt
+++ data/src/main/java/com/example/data/local/dao/TaskDao.kt
@@ -3,15 +3,22 @@ package com.example.data.local.dao
 import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
 import com.example.data.local.entity.TaskLocal
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
 
 @Dao
 interface TaskDao {
 
-    @Insert
-    fun insert(tasks: TaskLocal)
+    @Query("SELECT * FROM task")
+    fun getAllTasks(): Flow<List<TaskLocal>>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(task: TaskLocal): Long
 
     @Delete
-    fun delete(task: TaskLocal)
+    suspend fun delete(task: TaskLocal): Int
 
 }
