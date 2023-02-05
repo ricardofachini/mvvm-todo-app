09b761d10783ec252d3df8c6fc3af00b250ea2a8
Ricardo Fachini <ricardofachinisouza@gmail.com>
2023-02-04 22:00:03 -0300

    implements tasks viewmodel with add, retreive and delete methods.

--- app/src/main/java/com/example/todoapp/home/TodoListViewModel.kt
+++ app/src/main/java/com/example/todoapp/home/TodoListViewModel.kt
@@ -1,18 +1,51 @@
 package com.example.todoapp.home
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.domain.model.Task
+import com.example.domain.usecase.AddTaskUseCase
+import com.example.domain.usecase.DeleteTaskUseCase
+import com.example.domain.usecase.GetTasksUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class TodoListViewModel(): ViewModel() {
+@HiltViewModel
+class TodoListViewModel @Inject constructor(
+    private val addTaskUseCase: AddTaskUseCase,
+    private val getTasksUseCase: GetTasksUseCase,
+    private val deleteTaskUseCase: DeleteTaskUseCase
+): ViewModel() {
 
-    private val tasksList = mutableListOf<Task>()
+    val tasksList = MutableLiveData<List<Task>>()
 
+    /**
+     * Adds a new task to the repositories
+     */
     fun addTask(task: Task) {
-        tasksList.add(task)
+        viewModelScope.launch {
+            val result = addTaskUseCase.invoke(task)
+            println("resultado addtask: $result")
+            if (!result) {
+                println("Erro ao inserir")
+            }
+        }
     }
 
-    fun getList(): MutableList<Task> {
-        return tasksList
+    fun delete(id: Int) {
+        viewModelScope.launch {
+            val result = deleteTaskUseCase.delete(id)
+        }
+    }
+
+
+    fun getAllTasks() {
+        getTasksUseCase.call().onEach { data ->
+            this.tasksList.value = data
+        }.launchIn(viewModelScope)
     }
 
 }
