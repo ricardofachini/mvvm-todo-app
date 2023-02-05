0439dc10d0794194932aa184e967270993da827f
Ricardo Fachini <ricardofachinisouza@gmail.com>
2023-02-04 21:53:33 -0300

    receive task Object from viewmodel

--- domain/src/main/java/com/example/domain/usecase/AddTaskUseCase.kt
+++ domain/src/main/java/com/example/domain/usecase/AddTaskUseCase.kt
@@ -3,15 +3,11 @@ package com.example.domain.usecase
 import com.example.domain.model.Task
 import com.example.domain.repository.ITaskRepositoryLocal
 
-class AddTaskUseCase(
+class AddTaskUseCase constructor(
     private val taskLocalRepositoryLocal: ITaskRepositoryLocal
 ) {
 
-    suspend fun invoke(): Boolean {
-        return taskLocalRepositoryLocal.insertNewTask(Task(
-            id = 0,
-            title = "tarefa",
-            isDone = false
-        ))
+    suspend fun invoke(task: Task): Boolean {
+        return taskLocalRepositoryLocal.insertNewTask(task)
     }
 }
