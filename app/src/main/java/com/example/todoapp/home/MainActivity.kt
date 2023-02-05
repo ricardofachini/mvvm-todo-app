4b198235b634b089b489566d5008c718db6b6e81
Ricardo Fachini <ricardofachinisouza@gmail.com>
2023-02-04 21:58:19 -0300

    implements taskslist observer and updates the ui with every change that occurs in the repository

--- app/src/main/java/com/example/todoapp/home/MainActivity.kt
+++ app/src/main/java/com/example/todoapp/home/MainActivity.kt
@@ -3,24 +3,34 @@ package com.example.todoapp.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.domain.model.Task
 import com.example.todoapp.databinding.ActivityMainBinding
 import com.example.todoapp.home.adapter.TasksAdapter
 import com.example.todoapp.home.newtask.NewTaskDialogFragment
 import com.example.todoapp.home.newtask.NewTaskDialogListener
-import org.koin.android.ext.android.inject
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity(), NewTaskDialogListener {
     private lateinit var binding: ActivityMainBinding
-    private val viewModel: TodoListViewModel by inject()
-
+    private val viewModel: TodoListViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setupView()
+        val tasksAdapter = TasksAdapter()
+        binding.todoList.adapter = tasksAdapter
+        binding.todoList.layoutManager = LinearLayoutManager(this)
+        viewModel.getAllTasks()
+        viewModel.tasksList.observe(this, Observer { data ->
+            tasksAdapter.submitList(data)
+            tasksAdapter.notifyDataSetChanged()
+        })
     }
 
     private fun setupView() {
@@ -38,10 +48,6 @@ class MainActivity : AppCompatActivity(), NewTaskDialogListener {
             title = title,
             isDone = false
         )
-        val tasksAdapter = TasksAdapter()
-        binding.todoList.adapter =tasksAdapter
-        binding.todoList.layoutManager = LinearLayoutManager(this)
         viewModel.addTask(task)
-        tasksAdapter.submitList(viewModel.getList())
     }
 }
