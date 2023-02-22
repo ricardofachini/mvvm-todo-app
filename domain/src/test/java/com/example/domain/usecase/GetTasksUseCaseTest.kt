902f0a44665880fae37b4c4bd5f1fb1f6d61826c
Ricardo Fachini <ricardofachinisouza@gmail.com>
2023-02-22 16:06:16 -0300

    implements unit test for get task use case

---
+++ domain/src/test/java/com/example/domain/usecase/GetTasksUseCaseTest.kt
@@ -0,0 +1,34 @@
+package com.example.domain.usecase
+
+import com.example.domain.model.Task
+import com.example.domain.repository.ITaskRepositoryLocal
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import org.hamcrest.core.IsInstanceOf.instanceOf
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.whenever
+
+class GetTasksUseCaseTest {
+    private lateinit var useCase: GetTasksUseCase
+
+    @Before
+    fun setupTest() {
+        val repositoryMock: ITaskRepositoryLocal = mock()
+        val task = com.example.domain.model.Task(1, "task", false)
+        val actual = mutableListOf<com.example.domain.model.Task>()
+        actual.add(task)
+        whenever(repositoryMock.getAllTasks()).thenReturn( flow {
+            emit(actual)
+        })
+        useCase = GetTasksUseCase(repositoryMock)
+    }
+
+    @Test
+    fun `test get tasks`() {
+        val result = useCase.call()
+        assertThat(result, instanceOf(Flow::class.java))
+    }
+}
