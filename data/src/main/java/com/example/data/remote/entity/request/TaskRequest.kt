b0d2798178c6eb2f753616a4fbd1bf8ac5fd1d6f
Ricardo Fachini <ricardofachinisouza@gmail.com>
2024-08-09 12:05:59 -0300

    first tasks network dataclasses

--- data/src/main/java/com/example/data/remote/entity/request/TaskRequest.kt
+++ data/src/main/java/com/example/data/remote/entity/request/TaskRequest.kt
@@ -1,9 +1,8 @@
-package com.example.data.remote.entity.response
+package com.example.data.remote.entity.request
 
 import com.squareup.moshi.Json
 
-data class TaskResponse(
-    @field:Json(name = "id") val taskId: Int,
+data class TaskRequest (
     @field:Json(name = "title") val title: String,
     @field:Json(name = "description") val description: String,
     @field:Json(name = "is_completed") val isDone: Boolean,
