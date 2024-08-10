114c3df48db633c1f95a7fea8fa396823a6c30bf
Ricardo Fachini <ricardofachinisouza@gmail.com>
2024-08-10 14:00:13 -0300

    add logging in retrofit

--- data/src/main/java/com/example/data/remote/NetworkModule.kt
+++ data/src/main/java/com/example/data/remote/NetworkModule.kt
@@ -6,6 +6,8 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import javax.inject.Singleton
@@ -22,11 +24,20 @@ object NetworkModule {
         return "http://10.0.2.2:8000/api/"
     }
 
+    val interceptor : HttpLoggingInterceptor = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    val client : OkHttpClient = OkHttpClient.Builder().apply {
+        addInterceptor(interceptor)
+    }.build()
+
     @Provides
     @Singleton
     fun provideRetrofit(baseUrl: String): Retrofit = Retrofit.Builder()
         .baseUrl(baseUrl)
         .addConverterFactory(MoshiConverterFactory.create(providesMoshi()))
+        .client(client)
         .build()
 
     @Provides
