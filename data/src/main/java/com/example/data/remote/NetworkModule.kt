495c4b90efe0204f614c591e879b958065257977
Ricardo Fachini <ricardofachinisouza@gmail.com>
2024-08-10 11:51:38 -0300

    provides moshi builder

--- data/src/main/java/com/example/data/remote/NetworkModule.kt
+++ data/src/main/java/com/example/data/remote/NetworkModule.kt
@@ -1,5 +1,7 @@
 package com.example.data.remote
 
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -17,14 +19,20 @@ object NetworkModule {
 
     @Provides
     fun providesBaseUrl(): String {
-        return "http://127.0.0.1:8000/api/"
+        return "http://10.0.2.2:8000/api/"
     }
 
     @Provides
     @Singleton
     fun provideRetrofit(baseUrl: String): Retrofit = Retrofit.Builder()
         .baseUrl(baseUrl)
-        .addConverterFactory(MoshiConverterFactory.create())
+        .addConverterFactory(MoshiConverterFactory.create(providesMoshi()))
+        .build()
+
+    @Provides
+    @Singleton
+    fun providesMoshi(): Moshi = Moshi.Builder()
+        .add(KotlinJsonAdapterFactory())
         .build()
 
     @Singleton
